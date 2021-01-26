c6e36ea261880d4dd45b425b9924035e795606f5
Vokler <valerypavlikov@yandex.ru>
2021-01-26 12:46:55 +0300

    lesson3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,27 +1,15 @@
 from django.shortcuts import render
 
+from mainapp.models import Product, ProductCategory
+
+
 def index(request):
     return render(request, 'mainapp/index.html')
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
-
-
-def test_context(request):
     context = {
-        'title': 'test Context',
-        'header': 'Добро пожаловать на сайт!',
-        'username': 'Валера Павликов',
-        'products': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': '6 090,00'},
-            {'name': 'Синяя куртка The North Face', 'price': '23 725,00'},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': '3 390,00'},
-        ],
-        'promotion': False,
-        'products_of_promotion': [
-            {'name': 'Черный рюкзак Nike Heritage', 'price': '2 340,00'},
-            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': '13 590,00'},
-        ]
+        'products': Product.objects.all(),
+        'categories': ProductCategory.objects.all(),
     }
-    return render(request, 'mainapp/context.html', context)
+    return render(request, 'mainapp/products.html', context)
