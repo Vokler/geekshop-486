9d8e4ed493c8c6d93a7f387a6c8740910b871fdb
Vokler <valerypavlikov@yandex.ru>
2021-02-02 10:58:52 +0300

    added basket app

--- authapp/views.py
+++ authapp/views.py
@@ -3,8 +3,7 @@ from django.contrib import auth
 from django.urls import reverse
 
 from authapp.forms import UserLoginForm, UserRegisterForm, UserProfileForm
-from authapp.models import User
-
+from basket.models import Basket
 
 def login(request):
     if request.method == 'POST':
@@ -47,5 +46,8 @@ def profile(request):
             return HttpResponseRedirect(reverse('auth:profile'))
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'form': form}
+    context = {
+        'form': form,
+        'baskets': Basket.objects.filter(user=request.user),
+    }
     return render(request, 'authapp/profile.html', context)
