0d3dbcff6455a9f332d9a3064be91762702b02f0
Vokler <valerypavlikov@yandex.ru>
2021-02-08 21:32:09 +0300

    created update and create controllers for admin

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -7,4 +7,6 @@ app_name = 'adminapp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin-users-read/', views.admin_users_read, name='admin_users_read'),
+    path('admin-users-create/', views.admin_users_create, name='admin_users_create'),
+    path('admin-users-update/<int:id>/', views.admin_users_update, name='admin_users_update'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
 from authapp.models import User
+from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
 
 
 def index(request):
@@ -10,3 +12,30 @@ def index(request):
 def admin_users_read(request):
     context = {'users': User.objects.all()}
     return render(request, 'adminapp/admin-users-read.html', context)
+
+
+def admin_users_create(request):
+    if request.method == 'POST':
+        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users_read'))
+        else:
+            print(form.errors)
+    else:
+        form = UserAdminRegisterForm()
+    context = {'form': form}
+    return render(request, 'adminapp/admin-users-create.html', context)
+
+
+def admin_users_update(request, id):
+    user = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users_read'))
+    else:
+        form = UserAdminProfileForm(instance=user)
+    context = {'form': form, 'current_user': user}
+    return render(request, 'adminapp/admin-users-update-delete.html', context)
